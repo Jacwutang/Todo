79fee5ce996cea1526018b8cb973ad9e8f503d7e
Jack Wu <jackwu350@yahoo.com>
2018-03-28 12:09:20 -0700

    breakpoint at clearItem function

--- index.js
+++ index.js
@@ -32,13 +32,18 @@ window.onload = () => {
   input.placeholder = "Enter Todo Item";
   input.type = "text"
 
+  let array = [];
   input.addEventListener('keyup', (e) => {
 
+
     if(e.keyCode === 13 ){
+      let li = "<li>" + e.target.value + "<button id='delete' onclick='clearItem()'> Delete </button> </li>";
+      ul.innerHTML += li;
+      array.push(li);
 
-      ul.innerHTML += "<li>" + e.target.value + "<button id='delete' onclick='clearItem()'> Delete </button> </li>";
+      window.localStorage.setItem("items", JSON.stringify(array));
 
-
+      //Reset input field
       input.value = ""
     } else{
 
@@ -56,4 +61,29 @@ window.onload = () => {
 function clearItem(){
 
   event.currentTarget.parentElement.remove();
+
+  let items = JSON.parse(window.localStorage.getItem("items"));
+
+  let index = 0;
+
+
+  for( var i in items){
+      console.log(event.currentTarget.parentElement, i, items[i]);
+      if(JSON.stringify(event.currentTarget.parentElement) === items[i]){
+
+        return;
+
+      }
+
+
+  }
+
+  items.splice(i,1);
+  // console.log(items);
+
+  window.localStorage.setItem("items", JSON.stringify(items));
+
+
+
+
 }
